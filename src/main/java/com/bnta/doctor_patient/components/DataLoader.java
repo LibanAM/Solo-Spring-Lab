5580445f72f9e706fde77b9901ce08a2a57fe7fd
Liban Mohamed <libanam7@gmail.com>
2022-05-19 00:11:32 +0100

    Updated DataLoader with more patients

--- src/main/java/com/bnta/doctor_patient/components/DataLoader.java
+++ src/main/java/com/bnta/doctor_patient/components/DataLoader.java
@@ -29,12 +29,13 @@ public class DataLoader implements ApplicationRunner {
         Doctor doctor3 = new Doctor("Dr.Andy");
         doctorRepository.saveAll(Arrays.asList(doctor1, doctor2, doctor3));
 
-        Patient patient1 = new Patient("John Doe", 27, "johndoe27@gmail.com",
-                "Broken foot", doctor1);
-        Patient patient2 = new Patient("Jane Doe", 55, "janedoe55@outlook.com",
-                "Flu-like symptoms", doctor1);
-//        Patient patient3 = new Patient("")
-        patientRepository.saveAll(Arrays.asList(patient1, patient2));
+        Patient patient1 = new Patient("John Doe", 27, "johndoe27@gmail.com", "Broken foot", doctor1);
+        Patient patient2 = new Patient("Jane Doe", 55, "janedoe55@outlook.com", "Flu-like symptoms", doctor1);
+        Patient patient3 = new Patient("Mike Myers", 58, "halloween1978@outlook.com", "Persistent cough", doctor1);
+        Patient patient4 = new Patient("Jason Voorhees", 75, "jvoorhees@yahoo.com", "Crippling Migraines", doctor2);
+        Patient patient5 = new Patient("Chucky Ray", 19, "chuckythedoll@aol.com", "Sprained ankle", doctor2);
+        Patient patient6 = new Patient("Barack Obama", 44, "POTUS44@aol.com", "Back pain", doctor3);
+        patientRepository.saveAll(Arrays.asList(patient1, patient2, patient3, patient4, patient5, patient6));
 
     }
 }
