a2046fbb0a32192815a43c1785632a4c55a65798
Liban Mohamed <libanam7@gmail.com>
2022-05-18 21:56:43 +0100

    Initialised spring boot file

---
+++ src/main/java/com/bnta/doctor_patient/DoctorPatientApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.doctor_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientApplication.class, args);
+	}
+
+}
