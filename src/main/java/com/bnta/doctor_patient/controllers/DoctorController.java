9765a0c0a3c2115b0e65a1f61bf1ecb7d1c11019
Liban Mohamed <libanam7@gmail.com>
2022-05-19 01:47:21 +0100

    Updated DoctorController with Post, Show and Delete mapping

--- src/main/java/com/bnta/doctor_patient/controllers/DoctorController.java
+++ src/main/java/com/bnta/doctor_patient/controllers/DoctorController.java
@@ -1,12 +1,14 @@
 package com.bnta.doctor_patient.controllers;
 
 import com.bnta.doctor_patient.models.Doctor;
+import com.bnta.doctor_patient.models.Patient;
 import com.bnta.doctor_patient.repositories.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
 import java.util.List;
 
 @RestController
@@ -27,7 +29,20 @@ public class DoctorController {
     public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id){
         return new ResponseEntity(doctorRepository.findById(id), HttpStatus.OK);
     }
-    
+
+    //POST
+    @PostMapping("/new")
+    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor newDoctor){
+        Doctor doctor = doctorRepository.save(newDoctor);
+        return new ResponseEntity<>(doctor, HttpStatus.CREATED);
+    }
+
+    // DELETE
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<Long> deleteDoctor(@PathVariable Long id) {
+        doctorRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.NOT_FOUND);
+    }
 
 
 }
